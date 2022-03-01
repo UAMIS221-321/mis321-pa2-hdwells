292df31c9994a21cd335ad8fad65511fd14ca5fb
Hannah <hdwells@crimson.ua.edu>
2022-02-28 18:38:34 -0600

    new changes

--- Program.cs
+++ Program.cs
@@ -9,108 +9,107 @@ namespace mis321_pa2_hdwells
         {
             Ship();
             WelcomeMessage();
-        
+            Chooseplayer1();
+
         }
 
-  public static void WelcomeMessage()
+        public static void WelcomeMessage()
         {
             System.Console.WriteLine("Welcome to the Battle of Calypso's Maelstrom Game! ");
-            System.Console.WriteLine("");
-            Chooseplayer1();
+            
         }
 
         static void Ship()
-    {
-        for (int i = 0; i < 5; i++)
         {
-            for (int j = 0; j < 5; j++)
+            for (int i = 0; i < 5; i++)
             {
-                Console.Clear();
-
-                // steam
-                
-                Console.Write("       . . . . o o o o o o", Color.LightGray);
-                for (int s = 0; s < j / 2; s++)
+                for (int j = 0; j < 5; j++)
                 {
-                    Console.Write(" o", Color.LightGray);
+                    Console.Clear();
+
+                    // steam
+
+                    Console.Write("       . . . . o o o o o o", Color.LightGray);
+                    for (int s = 0; s < j / 2; s++)
+                    {
+                        Console.Write(" o", Color.LightGray);
+                    }
+                    Console.WriteLine();
+                    var margin = "".PadLeft(j);
+
+                    margin = "".PadLeft(j);
+                    Console.WriteLine(margin + "                    |~     ", Color.DarkGreen);
+                    Console.WriteLine(margin + "                    |/     |>   |>", Color.DarkGreen);
+                    Console.WriteLine(margin + "                    / (   (|    |", Color.DarkGreen);
+                    Console.WriteLine(margin + "                  /( (/   |)    |>", Color.DarkGreen);
+                    Console.WriteLine(margin + "            ____  ( (/    (|    | )  ,", Color.DarkGreen);
+                    Console.WriteLine(margin + "           |----|  (/ |   /|    |'| /^;", Color.DarkGreen);
+                    Console.WriteLine(margin + "            \\---*---Y--+-----+---+--//(;", Color.Magenta);
+                    Console.WriteLine(margin + "             \\-------*---*--*---*--//;", Color.Magenta);
+                    Console.WriteLine("   ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ", Color.DarkBlue);
+
+                    Thread.Sleep(200);
                 }
-                Console.WriteLine();
-                var margin = "".PadLeft(j);
-
-                margin = "".PadLeft(j);
-                Console.WriteLine(margin + "                    |~     ", Color.DarkGreen);
-                Console.WriteLine(margin + "                    |/     |>   |>", Color.DarkGreen);
-                Console.WriteLine(margin + "                    / (   (|    |" , Color.DarkGreen);
-                Console.WriteLine(margin + "                  /( (/   |)    |>", Color.DarkGreen);
-                Console.WriteLine(margin + "            ____  ( (/    (|    | )  ," , Color.DarkGreen);
-                Console.WriteLine(margin + "           |----|  (/ |   /|    |'| /^;", Color.DarkGreen);
-                Console.WriteLine(margin + "            \\---*---Y--+-----+---+--//(;", Color.Magenta);
-                Console.WriteLine(margin + "             \\-------*---*--*---*--//;", Color.Magenta);
-                Console.WriteLine("   ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ", Color.DarkBlue);
-
-                Thread.Sleep(200);
             }
         }
-    }
 
-        //randomize character - extra???
         public static void Chooseplayer1()
         {
             Character player1 = new Character();
 
             System.Console.WriteLine("Player 1 please enter your name! ");
             player1.Name = System.Console.ReadLine();
-            
+
 
             System.Console.WriteLine($"{player1.Name} please choose your character or exit the system");
             System.Console.WriteLine("\n1. Jack Sparrow \n2. Will Turner \n3. Davy Jones \n4. Elizabeth Swann \n5. Captain Barbosa \n6. Choose Character at Random \n7. Exit the Game ");
             int chooseCharacter1 = int.Parse(Console.ReadLine());
-            while(chooseCharacter1 > 7 && chooseCharacter1 < 1)
+            while (chooseCharacter1 > 7 && chooseCharacter1 < 1)
             {
                 Console.WriteLine("Invalid input! Please choose a number between 1-6.");
                 chooseCharacter1 = int.Parse(Console.ReadLine());
             }
 
-            if(chooseCharacter1 == 1)
+            if (chooseCharacter1 == 1)
             {
                 player1.characterType = 1;
                 player1.Health = 100;
                 player1.SetAttackBehavior(new Distract());
                 player1.MaxPower = GetMaxPower();
             }
-            else if(chooseCharacter1 == 2 )
+            else if (chooseCharacter1 == 2)
             {
-                player1.characterType = 2; 
-                player1.Health = 100; 
+                player1.characterType = 2;
+                player1.Health = 100;
                 player1.SetAttackBehavior(new Sword());
                 player1.MaxPower = GetMaxPower();
             }
-            else if(chooseCharacter1 == 3)
+            else if (chooseCharacter1 == 3)
             {
-                player1.characterType = 3; 
-                player1.Health = 100;  
+                player1.characterType = 3;
+                player1.Health = 100;
                 player1.SetAttackBehavior(new Cannon());
                 player1.MaxPower = GetMaxPower();
             }
-            else if (chooseCharacter1 == 4 )
+            else if (chooseCharacter1 == 4)
             {
-                player1.characterType = 4; 
+                player1.characterType = 4;
                 player1.SetAttackBehavior(new Stun());
-                player1.Health = 100; 
-                player1.MaxPower = GetMaxPower(); 
+                player1.Health = 100;
+                player1.MaxPower = GetMaxPower();
             }
-            else if (chooseCharacter1 == 5 )
+            else if (chooseCharacter1 == 5)
             {
-                player1.characterType = 5; 
-                player1.Health = 100;  
+                player1.characterType = 5;
+                player1.Health = 100;
                 player1.SetAttackBehavior(new MonkeyAttack());
                 player1.MaxPower = GetMaxPower();
             }
-            else if (chooseCharacter1 == 6 )
+            else if (chooseCharacter1 == 6)
             {
                 Random r = new Random();
-                player1.characterType = r.Next(0,5);  
-                player1.Health = 100; 
+                player1.characterType = r.Next(0, 5);
+                player1.Health = 100;
                 player1.MaxPower = GetMaxPower();
 
                 if (player1.characterType == 1)
@@ -138,73 +137,74 @@ namespace mis321_pa2_hdwells
             {
                 System.Environment.Exit(0);
             }
-            else{
+            else
+            {
                 System.Console.WriteLine("Invalid input! Please start over.");
                 Console.Clear();
                 Chooseplayer1();
             }
             Chooseplayer2(player1);
-            
+
         }
 
         public static void Chooseplayer2(Character player1)
         {
             Console.Clear();
-            Character player2 = new Character(); 
+            Character player2 = new Character();
 
             System.Console.WriteLine("Player 2 please enter your name! ");
             player2.Name = System.Console.ReadLine();
-        
+
             System.Console.WriteLine($"{player2.Name} please choose your character or exit the system");
             System.Console.WriteLine("\n1. Jack Sparrow \n2. Will Turner \n3. Davy Jones \n4. Elizabeth Swann \n5. Captain Barbosa \n6. Choose Character at Random \n7. Exit the Game ");
-            
+
             int chooseCharacter2 = int.Parse(Console.ReadLine());
-            while(chooseCharacter2 > 6 && chooseCharacter2 < 1)
+            while (chooseCharacter2 > 6 && chooseCharacter2 < 1)
             {
                 Console.WriteLine("Invalid input! Please choose a number between 1-6.");
                 chooseCharacter2 = int.Parse(Console.ReadLine());
             }
 
-            if(chooseCharacter2 == 1)
+            if (chooseCharacter2 == 1)
             {
                 player2.characterType = 1;
-                player2.Health = 100; 
-                player2.SetAttackBehavior(new Distract()); 
+                player2.Health = 100;
+                player2.SetAttackBehavior(new Distract());
                 player2.MaxPower = GetMaxPower();
             }
-            else if(chooseCharacter2 == 2 )
+            else if (chooseCharacter2 == 2)
             {
-                player2.characterType = 2;  
-                player2.Health = 100; 
+                player2.characterType = 2;
+                player2.Health = 100;
                 player2.SetAttackBehavior(new Sword());
                 player2.MaxPower = GetMaxPower();
             }
-            else if(chooseCharacter2 == 3)
+            else if (chooseCharacter2 == 3)
             {
                 player2.characterType = 3;
-                player2.Health = 100;  
+                player2.Health = 100;
                 player2.SetAttackBehavior(new Cannon());
-                player2.MaxPower = GetMaxPower(); 
+                player2.MaxPower = GetMaxPower();
             }
-            else if (chooseCharacter2 == 4 )
+            else if (chooseCharacter2 == 4)
             {
-                player2.characterType = 4;  
-                player2.Health = 100; 
+                player2.characterType = 4;
+                player2.Health = 100;
                 player2.SetAttackBehavior(new Stun());
                 player2.MaxPower = GetMaxPower();
             }
-            else if (chooseCharacter2 == 5 )
+            else if (chooseCharacter2 == 5)
             {
-                player2.characterType = 5;  
-                player2.Health = 100; 
+                player2.characterType = 5;
+                player2.Health = 100;
                 player2.SetAttackBehavior(new MonkeyAttack());
                 player2.MaxPower = GetMaxPower();
             }
-            else if (chooseCharacter2 == 6 )
+            else if (chooseCharacter2 == 6)
             {
                 Random r = new Random();
-                player2.characterType = r.Next(0,5);  
-                player2.Health = 100; 
+                player2.characterType = r.Next(0, 5);
+                player2.Health = 100;
                 player2.MaxPower = GetMaxPower();
 
                 if (player2.characterType == 1)
@@ -234,92 +234,92 @@ namespace mis321_pa2_hdwells
             }
 
             GameTime(player1, player2);
-            
+
         }
 
-       public static double GetMaxPower()
+        public static double GetMaxPower() //randomizes max power
         {
             Random m = new Random();
-            double MaxPower = m.Next(1,100);
+            double MaxPower = m.Next(1, 100);
             return MaxPower;
         }
 
-        public static void GameTime(Character player1, Character player2)
+        public static void GameTime(Character player1, Character player2) //start of the actual game
         {
-            System.Console.WriteLine("Let's play the Battle of Calypso's Maelstrom! \t.");
-            System.Console.WriteLine("************************************************ \t.");
+            System.Console.WriteLine("Let's play the Battle of Calypso's Maelstrom! \t");
+            System.Console.WriteLine("************************************************ \t");
 
             Random r = new Random();
-            int whosTurn = r.Next(0,2);
+            int whosTurn = r.Next(0, 2);
 
-            if(whosTurn == 1)
+            if (whosTurn == 1)
             {
 
-              while(player1.Health > 0 && player2.Health > 0  )  
-              {
-                
-               System.Console.WriteLine($"{player1.Name} is attacking . . .");
-               double DefensePower = player1.GetDefensePower();
-               double AttackPower = player1.GetAttackPower(player1.MaxPower);
-               double attack = player1.attackBehavior.Attack(player2, AttackPower, DefensePower);
-               player2.Health = player2.Health - attack;
-               System.Console.WriteLine($"Power done: {AttackPower} | Defense Power: {DefensePower} | Damage Dealt: {attack}");
-               ShowPlayerStats(player2);
-               ManageHealth(player2, player1);
+                while (player1.Health > 0 && player2.Health > 0)
+                {
 
+                    System.Console.WriteLine($"{player1.Name} is attacking ...");
+                    double DefensePower = player1.GetDefensePower();
+                    double AttackPower = player1.GetAttackPower(player1.MaxPower);
+                    double attack = player1.attackBehavior.Attack(player2, AttackPower, DefensePower);
+                    player2.Health = player2.Health - attack;
+                    System.Console.WriteLine($"Attack Power: {AttackPower} | Defense Power: {DefensePower} | Damage Dealt: {attack}");
+                    ShowPlayerStats(player2);
+                    ManageHealth(player2, player1);
 
-               System.Console.WriteLine($"{player2.Name} is attacking . . .");
-               DefensePower = player2.GetDefensePower();
-               AttackPower = player2.GetAttackPower(player2.MaxPower);
-               attack = player2.attackBehavior.Attack(player1, AttackPower, DefensePower);
-               player1.Health = player1.Health - attack;
-               System.Console.WriteLine($"Power done: {AttackPower} |  Defense Power: {DefensePower} | Damage Dealt: {attack}");
-               ShowPlayerStats(player1);
-               ManageHealth(player1, player2);
-              }
+                    System.Console.WriteLine($"{player2.Name} is attacking ...");
+                    DefensePower = player2.GetDefensePower();
+                    AttackPower = player2.GetAttackPower(player2.MaxPower);
+                    attack = player2.attackBehavior.Attack(player1, AttackPower, DefensePower);
+                    player1.Health = player1.Health - attack;
+                    System.Console.WriteLine($"Attack Power: {AttackPower} |  Defense Power: {DefensePower} | Damage Dealt: {attack}");
+                    ShowPlayerStats(player1);
+                    ManageHealth(player1, player2);
+                }
             }
             else
-            { 
-            while(player1.Health > 0 && player2.Health > 0)  
-              {
-                
-               System.Console.WriteLine($"{player2.Name} is attacking . . .");
-               double DefensePower = player2.GetDefensePower();
-               double AttackPower = player2.GetAttackPower(player2.MaxPower);
-               double attack = player2.attackBehavior.Attack(player1, AttackPower, DefensePower);
-               player1.Health = player1.Health - attack;
-               System.Console.WriteLine($"Attack Power: {AttackPower} | Defense Power: {DefensePower} | Damage Dealt: {attack}");
-               ShowPlayerStats(player1);
-               ManageHealth(player1, player2);
+            {
+                while (player1.Health > 0 && player2.Health > 0)
+                {
 
-                
-                
-               System.Console.WriteLine($"{player1.Name} is attacking . . .");
-               DefensePower = player1.GetDefensePower();
-               AttackPower = player1.GetAttackPower(player1.MaxPower);
-               attack = player1.attackBehavior.Attack(player2, AttackPower, DefensePower);
-               player2.Health = player2.Health - attack;
-               System.Console.WriteLine($"Attack power: {AttackPower} | Defense Power: {DefensePower} | Damage Dealt: {attack}");
-               ShowPlayerStats(player2);
-               ManageHealth(player2, player1);
-              }
-            } 
+                    System.Console.WriteLine($"{player2.Name} is attacking ...");
+                    double DefensePower = player2.GetDefensePower();
+                    double AttackPower = player2.GetAttackPower(player2.MaxPower);
+                    double attack = player2.attackBehavior.Attack(player1, AttackPower, DefensePower);
+                    player1.Health = player1.Health - attack;
+                    System.Console.WriteLine($"Attack Power: {AttackPower} | Defense Power: {DefensePower} | Damage Dealt: {attack}");
+                    ShowPlayerStats(player1);
+                    ManageHealth(player1, player2);
+
+                    System.Console.WriteLine($"{player1.Name} is attacking ...");
+                    DefensePower = player1.GetDefensePower();
+                    AttackPower = player1.GetAttackPower(player1.MaxPower);
+                    attack = player1.attackBehavior.Attack(player2, AttackPower, DefensePower);
+                    player2.Health = player2.Health - attack;
+                    System.Console.WriteLine($"Attack power: {AttackPower} | Defense Power: {DefensePower} | Damage Dealt: {attack}");
+                    ShowPlayerStats(player2);
+                    ManageHealth(player2, player1);
+                }
+
+            }
+
         }
 
-        public static void ShowPlayerStats(Character player)
+        static void ShowPlayerStats(Character player)
         {
             System.Console.WriteLine($"{player.Name}'s stats: \n~ Health: {player.Health} ~ \n~ Max Power: {player.MaxPower} ~ ");
             System.Console.WriteLine("******************************************************** \t. ");
         }
 
-        public static void ManageHealth(Character player, Character player1)
+        static void ManageHealth(Character player, Character player1)
         {
-            if(player.Health <= 0)
+            if (player.Health <= 0)
             {
-
                 Console.WriteLine($" <<<<< {player1.Name} has won the game! >>>>>");
-              
+                System.Console.WriteLine("******************************************************** \t. ");
                 System.Environment.Exit(0);
+                
+
             }
         }
     }
